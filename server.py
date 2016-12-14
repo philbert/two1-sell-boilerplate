22a1c383737e91afa54d2d75f3346d13815998d6
Philip Cheong <isctsf@gmail.com>
2016-12-14 21:37:09 +0000

    switch app to fileserver

--- server.py
+++ server.py
@@ -1,20 +1,56 @@
 import os
-import flask
+import json
+import random
+
+from flask import Flask
+from flask import request
+from flask import send_from_directory
+
 from two1.wallet import Wallet
 from two1.bitserv.flask import Payment
-from two1.sell.util.decorators import track_requests
+
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+# directory of the digital content we'd like to sell
+dir_path = '/home/twenty/workspace/fileserver/sellfiles'
+
+# get a list of the files in the directory
+file_list = os.listdir(dir_path)
+
+# simple content model: dictionary of files w/ random prices
+files = {}
+for file_id in range(len(file_list)):
+    files[file_id+1] = file_list[file_id], random.randrange(1000, 3000)
 
 
-app = flask.Flask(__name__)
-payment = Payment(app, Wallet())
+# endpoint to look up files to buy
+@app.route('/files')
+def file_lookup():
+    return json.dumps(files)
 
 
-@app.route('/hello')
-@payment.required(5000, server_url=os.environ.get("PAYMENT_SERVER_IP", None))  # sets up payment channel support
-@track_requests  # allows `21 sell status --detail` to show correct stats
-def hello():
-    return 'Hello, world'
+# return the price of the selected file
+def get_price_from_request(request):
+    id = int(request.args.get('selection'))
+    return files[id][1]
 
 
-if __name__ == "__main__":
-    app.run(host="::", port=5000)  # each app must run on port 5000 for router container to reach it successfully
+# machine-payable endpoint that returns selected file if payment made
+@app.route('/buy')
+@payment.required(get_price_from_request)
+def buy_file():
+
+    # extract selection from client request
+    sel = int(request.args.get('selection'))
+
+    # check if selection is valid
+    if(sel < 1 or sel > len(file_list)):
+        return 'Invalid selection.'
+    else:
+        return send_from_directory(dir_path, file_list[int(sel)-1])
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(host='::', port='5000')
--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setuptools.setup(
     install_requires=[  # list all dependencies for your project here
         "Flask==0.10.1",  # required for hello21/server.py
         "PyYAML==3.11",  # required for scripts under hello21/utils
+        "two1==3.10.3",  # required for scripts under hello21/utils
     ],
 )
